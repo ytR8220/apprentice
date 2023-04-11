f12cc98dc464d11c21fa665c7b5b4f051c289851
yuto <yto.tamashiro@gmail.com>
2023-04-11 17:26:38 +0900

    fizzbuzzプログラムの作成

---
+++ ruby/fizzbuzz.rb
@@ -0,0 +1,20 @@
+# fizzbuzzをプログラムの作成
+def fizz_buzz(num)
+  if num % 15 == 0
+    puts 'FIzz Buzz'
+  elsif num % 5 == 0
+    puts 'Buzz'
+  elsif num % 3 == 0
+    puts 'Fizz'
+  else
+    puts num.to_s
+  end
+end
+
+fizz_buzz(1)
+fizz_buzz(2)
+fizz_buzz(3)
+fizz_buzz(4)
+fizz_buzz(5)
+fizz_buzz(6)
+fizz_buzz(15)
