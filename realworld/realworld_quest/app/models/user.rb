7ce14929e675d820d8aec2d96a79b798d37c5529
yuto <yto.tamashiro@gmail.com>
2023-07-01 23:13:23 +0900

    2023.7.1日報

--- realworld/realworld_quest/app/models/user.rb
+++ realworld/realworld_quest/app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   validates :username, :email, :password , presence: true
   has_secure_password
-  has_many :articles
-  dipendent: :destroy
+  has_many :articles, dependent: :destroy
 end
